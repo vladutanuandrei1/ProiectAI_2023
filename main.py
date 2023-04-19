cad3b23df5aed2f473d1e5cdc2d6140dd5254742
Radu-Mihail Ciuperca <ciuperca.i.radumihail19@student.rau.ro>
2023-04-20 00:13:50 +0300

    Trying without succes of exporting the array of pixels

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import gymnasium
 import numpy as np
 import agent
-
+import cv2
+import os
 
 # Initializare Environment
 
@@ -9,19 +10,25 @@ env = gymnasium.make("CarRacing-v2", render_mode="human")
 a = np.array([0.0, 0.0, 0.0])
 
 
+
 quit = False
 while not quit:
         env.reset()
+        # np.savetxt("output.csv", env.render(), '')
         total_reward = 0.0
         steps = 0
         restart = False
         while True:
             a =  agent.tokyoDrift(a)
             s, r, terminated, truncated, info = env.step(a)
+            
             total_reward += r
             if steps % 200 == 0 or terminated or truncated:
                 print("\naction " + str([f"{x:+0.2f}" for x in a]))
                 print(f"step {steps} total_reward {total_reward:+0.2f}")
+                # print("##########################################")
+                # print(env.render())
+                # print("##########################################")
             steps += 1
             if terminated or truncated or restart or quit:
                 break
